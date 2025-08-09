e848e0f54091967873add9dad056ef576982e76f
CtagaDev <ctagadev@proton.me>
2025-08-09 14:20:59 +0200

    Añadir petición edad + conversión a int

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -1,5 +1,12 @@
 ﻿string nombre;
+string edad;
+int edadInt;
 
 Console.WriteLine("Introduce tu nombre, por favor:");
 nombre = Console.ReadLine();
+Console.WriteLine("Introduce tu edad, por favor:");
+edad = Console.ReadLine();
+edadInt = Convert.ToInt32(edad);
+
 Console.WriteLine("¡Hola, " + nombre + "!");
+Console.WriteLine("Asi que tienes " + edadInt);
