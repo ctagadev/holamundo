f886ac4afe08a79957e6d9be10e4179e5d68f174
CtagaDev <ctagadev@proton.me>
2025-08-09 18:49:18 +0200

    Control de errores en la edad

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -1,14 +1,26 @@
 ﻿string nombre;
-string edad;
-int edadInt;
+int edadInt = 0;
 int añoActual = DateTime.Now.Year;
+bool edadInvalida = true;
 
 Console.WriteLine("Introduce tu nombre, por favor:");
 nombre = Console.ReadLine();
-Console.WriteLine("Introduce tu edad, por favor:");
-edad = Console.ReadLine();
-
-edadInt = Convert.ToInt32(edad);
+while (edadInvalida)
+{
+    try
+    {
+        string edad;
+        Console.WriteLine("Introduce tu edad, por favor:");
+        edad = Console.ReadLine();
+        edadInt = Convert.ToInt32(edad);
+        
+        edadInvalida = false;
+    } 
+    catch (FormatException e)
+    {
+        Console.WriteLine("La edad la tienes que poner de forma entera, sin decimales o letras.");
+    }
+}
 
 int cien = 100 - edadInt;
 int añoCien = añoActual + cien;
